aa39bc11f0711b6223f6481efc3410c545244fe2
david sullivan <david@swampmonster.org>
2021-10-09 21:46:49 -0500

    added keycommands and feedback

---
+++ Touchdesigner/KeyCommands.py
@@ -0,0 +1,12 @@
+Controlling and converting Information
+
+the flow of information goes from left to right
+Tab key brings up the OP create dialog
+Left mouse pans, middle mouse zooms
+the f key frames the selected object in the view
+the h key homes the view
+ u (for up) and i (for in) allow you to go in to some operators
+ you can also zoom into or out of operators
+ (option right click is like the middle mouse on trackpad, also two finger swipe)
+ 
+spacebar plays and pauses
