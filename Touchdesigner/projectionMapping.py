97e4c76f3dc6c0a9afe385fde5076bd3578f2987
david <david@swampmonster.org>
2021-10-16 16:53:42 -0500

    started adding 3d particle example

--- Touchdesigner/projectionMapping.py
+++ Touchdesigner/projectionMapping.py
@@ -3,7 +3,10 @@ Projection mapping
 To see the different ways to map in TD,
 go to the Dialogs menu and select Palette Browser.
 In the Palette Browser, select mapping.
-The 3 most used are camSchnapper, Kantan, and stoner.
+The 3 most used are 
+camSchnapper - for using with a 3d model of your projection surface, 
+Kantan - for quick 2d mapping, and 
+stoner - for grid warping (can be used with the other 2, as a final adjustment).
 Drag the one you want to use (or investigate) into
 your project.
 
